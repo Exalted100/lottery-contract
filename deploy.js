9dfe9ea877803f7d5676ef326a8c4f89949ad8da
Fawaz Haroun <harounayomikun@gmail.com>
2021-11-07 11:35:08 +0100

    yada

--- deploy.js
+++ deploy.js
@@ -2,9 +2,10 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const bytecode = require('./compile').evm.bytecode.object;
 const { abi } = require('./compile');
+const fs = require("fs");
 
 const provider = new HDWalletProvider (
-    "right fluid employ turtle quarter scene voice wreck silver master butter legal",
+    "insert mmenomics",
     "https://rinkeby.infura.io/v3/4a6ddb668c1b4c9d83d7149055df904e"
 )
 
@@ -18,6 +19,11 @@ const deploy = async () => {
         .deploy({ data: bytecode})
         .send({ gas: 1000000, from: accounts[0] })
 
+        console.log("The abi is: " + abi)
+        await fs.writeFile(`${__dirname}abi.txt`, JSON.stringify(abi), (err) => {
+            if (err) throw err;
+            console.log("the abi has been saved")
+        })
         console.log("Contract deployed to: " + result.options.address)
 }
 deploy()
